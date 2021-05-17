6a83784c84cda616417ad0f7c62851c5fa234e69
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2021-05-17 11:32:38 +0900

    Initial Commit

---
+++ APIPractice3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIPractice3
+//
+//  Created by 生田拓登 on 2021/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
